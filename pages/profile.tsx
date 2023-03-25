f05738f54f4279b84ef35c8eb4084da64a52efd4
62theories <nattawatx86@gmail.com>
2023-03-25 13:57:29 +0700

    add wagmi

---
+++ pages/profile.tsx
@@ -0,0 +1,27 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import {
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useEnsAvatar,
+  useEnsName,
+} from "wagmi";
+
+export default function Profile() {
+  const { address, connector, isConnected } = useAccount();
+  const { data: ensAvatar } = useEnsAvatar({ address });
+  const { data: ensName } = useEnsName({ address });
+  const { connect, connectors, error, isLoading, pendingConnector } =
+    useConnect();
+  const { disconnect } = useDisconnect();
+
+  if (isConnected) {
+    return <></>;
+  }
+
+  return (
+    <>
+      <ConnectButton />;
+    </>
+  );
+}
