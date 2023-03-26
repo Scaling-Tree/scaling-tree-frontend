7f968bc584946e3c9b2d7d2d39c80c46686aa41a
Warun S <warunsinx@gmail.com>
2023-03-26 17:05:45 +0700

    implemented leaderboard logic

---
+++ components/LeaderboardMilestone.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import Progressbar from "./Progressbar";
+import {
+  HiClipboardDocumentCheck,
+  HiClipboardDocumentList,
+} from "react-icons/hi2";
+import { nFormatter } from "@/utils/numberFormat";
+import { config } from "@/config";
+
+const GLOBAL_MILESTONE = config.globalMilestone;
+
+type PropsType = {
+  title: string;
+  currentMilestone: number;
+  isAudit: boolean;
+  onChange: (isAudit: boolean) => void;
+};
+
+export default function LeaderboardMilestone({
+  title,
+  currentMilestone,
+  isAudit,
+  onChange,
+}: PropsType) {
+  const remainingMilestone = Math.max(0, GLOBAL_MILESTONE - currentMilestone);
+  const percent = (currentMilestone / GLOBAL_MILESTONE) * 100;
+
+  return (
+    <div className="bg-white shadow-sm w-full mt-5 rounded-xl flex flex-col p-7 px-10">
+      <div className="mb-7 flex items-center justify-between">
+        <div>
+          <p className="font-bold text-2xl">{title}</p>
+          <p className="text-gray-400">Have you planted a tree today ?</p>
+        </div>
+        <div className="bg-gray-100 rounded-lg flex items-center justify-center p-1 space-x-3">
+          <div
+            onClick={() => onChange(true)}
+            className={`flex items-center p-2 px-3 rounded-lg cursor-pointer hover:opacity-90 ${
+              isAudit ? "text-gray-600 bg-white" : "text-gray-600 bg-gray-100"
+            }`}
+          >
+            <HiClipboardDocumentCheck />
+            <p className="ml-1 font-medium">Audited</p>
+          </div>
+          <div
+            onClick={() => onChange(false)}
+            className={`flex items-center p-2 px-3 rounded-lg cursor-pointer hover:opacity-90 ${
+              !isAudit ? "text-gray-600 bg-white" : "text-gray-600 bg-gray-100"
+            }`}
+          >
+            <HiClipboardDocumentList />
+            <p className="ml-1 font-medium">All</p>
+          </div>
+        </div>
+      </div>
+      <Progressbar percent={percent} />
+      <div className="flex items-center justify-between mt-2">
+        <p className="text-green-400 font-bold text-xl">
+          {nFormatter(currentMilestone)}
+        </p>
+        <p className="font-medium text-xl text-gray-400">
+          {nFormatter(remainingMilestone)} more to{" "}
+          <span className="text-green-400">{nFormatter(GLOBAL_MILESTONE)}</span>
+        </p>
+      </div>
+    </div>
+  );
+}
--- components/RankCard.tsx
+++ components/RankCard.tsx
@@ -1,21 +1,29 @@
 import React from "react";
 
-export default function RankCard() {
+export default function RankCard({
+  address,
+  rank,
+  amount,
+}: {
+  address: string;
+  rank: number;
+  amount: number;
+}) {
   return (
     <div className="w-full bg-white shadow mt-3 rounded-lg p-6 px-10 flex items-center">
       <div className="relative">
         <div className="w-20 h-20 bg-gray-300 rounded-lg" />
         <div className="w-7 h-7 bg-green-400 rounded-full absolute -top-2 -left-2 flex items-center justify-center text-white font-medium text-sm">
-          1
+          {rank}
         </div>
       </div>
       <div className="ml-5">
         <p className="font-medium text-lg text-gray-800">warunsinx</p>
-        <p className="text-gray-400">0x1ex0ababab...6Ab8</p>
+        <p className="text-gray-400">{address}</p>
       </div>
       <div className="flex items-center ml-auto">
         <div className="w-5 h-5 bg-green-700 rounded-full" />
-        <p className="ml-2 text-gray-400 text-lg">1.5 Trillion</p>
+        <p className="ml-2 text-gray-400 text-lg">{amount}</p>
       </div>
     </div>
   );
