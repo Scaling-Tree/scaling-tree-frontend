d614a161112cc4ca2799b564ee4226ac9a0b2572
Warun S <warunsinx@gmail.com>
2023-03-26 17:55:41 +0700

    update porifle module

--- Views/ProfileView.tsx
+++ Views/ProfileView.tsx
@@ -13,7 +13,7 @@ import {
   ethPersonalSign,
   ethPersonalSignRecoverPublicKey,
 } from "@polybase/eth";
-import * as React from "react";
+import React, { useState } from "react";
 import { useSignMessage } from "wagmi";
 import { verifyMessage } from "ethers/lib/utils";
 import { useIsMounted } from "@/hooks";
@@ -22,12 +22,43 @@ import { ethers } from "ethers";
 import ProfileBanner from "@/components/profile/ProfileBanner";
 import PersonalMilestone from "@/components/profile/PersonalMilestone";
 import TreeCard from "@/components/TreeCard";
+import { config } from "@/config";
+import { useQuery } from "urql";
+import { ProfileQueryDocument } from "@/.graphclient";
 
 export default function ProfileView() {
+  const { address, connector, isConnected } = useAccount();
+
+  const [page, setPage] = useState(0);
+  const [isAudit, setIsAudit] = useState(true);
+
+  const skip = page * config.pageSize;
+
+  const [result, reexecuteQuery] = useQuery({
+    query: ProfileQueryDocument,
+    variables: {
+      id: address,
+      first: config.pageSize,
+      skip,
+    },
+  });
+
+  const { data, fetching, error } = result;
+
+  const totalTrees = data?.app?.totalTrees || 0;
+  const auditedTrees = data?.app?.auditedTrees || 0;
+  const currentMilestone = isAudit ? auditedTrees : totalTrees;
+
+  console.log(data);
+
   return (
     <div className="max-w-[1000px] mx-auto">
       <ProfileBanner />
-      <PersonalMilestone />
+      <PersonalMilestone
+        isAudit={isAudit}
+        onChange={(value) => setIsAudit(value)}
+        currentMilestone={currentMilestone}
+      />
       {/* <TreeCard /> */}
       <div className="mt-5" />
     </div>
---
+++ hooks/useProfileWithAddr.ts
@@ -0,0 +1,80 @@
+import getDb from "@/utils/getDb";
+import { Polybase } from "@polybase/client";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import { useAccount, useSigner } from "wagmi";
+
+export interface Profile {
+  name: string;
+  profileImgUrl: string;
+}
+
+export function useProfileWithAddr(address: string) {
+  const { data: signer, isError, isLoading: isLoadingUseSigner } = useSigner();
+  const [profile, setProfile] = useState<Profile>({
+    name: "",
+    profileImgUrl: "",
+  });
+  // const { address, connector, isConnected } = useAccount();
+  useEffect(() => {
+    (async () => {
+      if (address) {
+        const db = getDb();
+        const collectionReference = db.collection("User");
+        const { data, block } = await collectionReference.record(address).get();
+        let profileToSet = { ...profile };
+        if (data?.name) {
+          profileToSet.name = data.name;
+        }
+        if (data?.profileImgUrl) {
+          profileToSet.profileImgUrl = data.profileImgUrl;
+        }
+        setProfile(profileToSet);
+      }
+    })();
+  }, [address]);
+
+  useEffect(() => {
+    (async () => {
+      if (address) {
+        try {
+          const db = getDb();
+          const user = await db.collection("User").record(address).get();
+        } catch (e) {
+          const db = getDb();
+          if (signer) {
+            db.signer(async (data: string) => {
+              return {
+                h: "eth-personal-sign",
+                sig: await signer.signMessage(data),
+              };
+            });
+            await db.collection("User").create([address]);
+            console.log("user created");
+          }
+        }
+      }
+    })();
+  }, [address, signer]);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     if (profile?.publicKey) {
+  //       const collectionReference = db.collection("User");
+  //       const { data, block } = await collectionReference
+  //         .record(profile.publicKey)
+  //         .get();
+  //       let profileToSet = { ...profile };
+  //       if (data?.name) {
+  //         profileToSet.name = data.name;
+  //       }
+  //       if (data?.profileImgUrl) {
+  //         profileToSet.profileImgUrl = data.profileImgUrl;
+  //       }
+  //       setProfile(profileToSet);
+  //     }
+  //   })();
+  // }, [profile?.publicKey]);
+
+  return { profile, setProfile };
+}
